3fbfd5b52ec996a6ac2246a0159d2ab0f5e907e9
Andrihery <roadsterandry@gmail.com>
2023-04-08 21:03:16 +0300

    Siege module & service done

--- src/siege/siege.module.ts
+++ src/siege/siege.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
 import { SiegeController } from './siege.controller';
 import { SiegeService } from './siege.service';
+import { SiegeSchema } from './schemas/siege.schema';
 
 @Module({
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([{ name: 'Siege', schema: SiegeSchema }]),
+  ],
   controllers: [SiegeController],
-  providers: [SiegeService]
+  providers: [SiegeService],
 })
 export class SiegeModule {}
--- src/siege/siege.service.ts
+++ src/siege/siege.service.ts
@@ -1,4 +1,75 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class SiegeService {}
+import {
+    BadRequestException,
+    Injectable,
+    NotFoundException,
+  } from '@nestjs/common';
+  import { InjectModel } from '@nestjs/mongoose';
+  import * as mongoose from 'mongoose';
+  import { Reservation } from './schemas/reservation.schema';
+  
+  import { Query } from 'express-serve-static-core';
+  import { User } from '../auth/schemas/user.schema';
+  
+  @Injectable()
+  export class ReservationService {
+    constructor(
+      @InjectModel(Reservation.name)
+      private reservationModel: mongoose.Model<Reservation>,
+    ) {}
+  
+    async findAll(query: Query): Promise<Reservation[]> {
+      const resPerPage = 2;
+      const currentPage = Number(query.page) || 1;
+      const skip = resPerPage * (currentPage - 1);
+  
+      const keyword = query.keyword
+        ? {
+            title: {
+              $regex: query.keyword,
+              $options: 'i',
+            },
+          }
+        : {};
+  
+      const reservations = await this.reservationModel
+        .find({ ...keyword })
+        .limit(resPerPage)
+        .skip(skip);
+      return reservations;
+    }
+  
+    async create(reservation: Reservation, user: User): Promise<Reservation> {
+      const data = Object.assign(reservation, { user: user._id });
+  
+      const res = await this.reservationModel.create(data);
+      return res;
+    }
+  
+    async findById(id: string): Promise<Reservation> {
+      const isValidId = mongoose.isValidObjectId(id);
+  
+      if (!isValidId) {
+        throw new BadRequestException('Please enter correct id.');
+      }
+  
+      const reservation = await this.reservationModel.findById(id);
+  
+      if (!reservation) {
+        throw new NotFoundException('Reservation not found.');
+      }
+  
+      return reservation;
+    }
+  
+    async updateById(id: string, reservation: Reservation): Promise<Reservation> {
+      return await this.reservationModel.findByIdAndUpdate(id, reservation, {
+        new: true,
+        runValidators: true,
+      });
+    }
+  
+    async deleteById(id: string): Promise<Reservation> {
+      return await this.reservationModel.findByIdAndDelete(id);
+    }
+  }
+  
