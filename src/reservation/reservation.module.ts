e328cbf2c7865ed54d490f9c0bf96f14d1a462fe
Andrihery <roadsterandry@gmail.com>
2023-04-08 18:59:08 +0300

    Reservation service & module done

--- src/reservation/reservation.module.ts
+++ src/reservation/reservation.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ReservationController } from './reservation.controller';
 import { ReservationService } from './reservation.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
+import { ReservationSchema } from './schemas/reservation.schema';
 @Module({
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([{ name: 'Reservation', schema: ReservationSchema }]),
+  ],
   controllers: [ReservationController],
   providers: [ReservationService]
 })
